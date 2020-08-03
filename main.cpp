3c348e0f8d7ffabb779aedcf78f619e042ac7b1c
ryanscol7 <64745177+ryanscol7@users.noreply.github.com>
2020-08-03 19:14:08 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "TreeMap.h"
+#include "HashMap.h"
+using namespace std;
+
+
+int main() {
+    TreeMap<int,vector<int>> treeMap;
+    HashMap<int,vector<int>> hashMap;
+
+    bool selection = true; //hash or tree map
+
+    readTaxiData(selection);
+    readUberData(selection);
+
+    return 0;
+}
+
+void readTaxiData(bool treeSelected){
+    ifstream taxiData("TaxiTripDataCSV.csv");
+
+    string pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix, line;
+    istringstream line_stream;
+
+
+    for(int i = 1; i< 8001; i++){
+        vector<int> tripInfo;
+        getline(uberData, line, '\n');
+        line_stream.str(line);
+        getline(line_stream,, ",");
+        getline(line_stream, pickupFull, ",");
+        for(int j = 0; j < pickupFull.length; j++){
+            if(pickupFull[i] != '/' && ' ' && ':'){
+                pickupFix += pickupFull[i];
+            }
+        }
+        int pickup = stoi(pickupFix);
+        getline(line_stream, dropoffFull, ",");
+        for(int k = 0; k < dropoffFull.length; k++){
+            if(dropoffFull[i] != '/' && ' ' && ':'){
+                dropoffFix += dropoffFull[i];
+            }
+        }
+        int dropoff = stoi(dropoffFix);
+        tripInfo.push_back(dropoff);
+        getline(line_stream,, ",");
+        getline(line_stream,, ",");
+        getline(line_stream,, ",");
+        getline(line_stream,, ",");
+        getline(line_stream, pickupID, ",");
+        int pickupIDint = stoi(pickupID);
+        tripInfo.push_back(pickupIDint);
+        getline(line_stream, dropoffID, ",");
+        int dropoffIDint = stoi(dropoffID);
+        tripInfo.push_back(dropoffIDint);
+
+        if(treeSelected == true)
+            treeMap[pickup] = tripInfo;
+        else
+            hashMap[pickup] = tripInfo;
+    }
+
+    taxiData.close();
+}
+
+void readUberData(){
+    ifstream uberData("UberTripDataCSV.csv");
+
+    string licenseNum, dispatchBase, pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix, line;
+    istringstream line_stream;
+
+    for(int i = 1; i< 8001; i++){
+        vector<int> tripInfo;
+        getline(uberData, line, '\n');
+        line_stream.str(line);
+        getline(line_stream, licenseNum, ",");
+        if(licenseNum != "HV0003"){
+            break;
+        }
+        getline(line_stream, dispatchBase, ",");
+        getline(line_stream, pickupFull, ",");
+        for(int j = 0; j < pickupFull.length; j++){
+            if(pickupFull[i] != '/' && ' ' && ':'){
+                pickupFix += pickupFull[i];
+            }
+        }
+        int pickup = stoi(pickupFix);
+        getline(line_stream, dropoffFull, ",");
+        for(int k = 0; k < dropoffFull.length; k++){
+            if(dropoffFull[i] != '/' && ' ' && ':'){
+                dropoffFix += dropoffFull[i];
+            }
+        }
+        int dropoff = stoi(dropoffFix);
+        tripInfo.push_back(dropoff);
+        getline(line_stream, pickupID, ",");
+        int pickupIDint = stoi(pickupID);
+        tripInfo.push_back(pickupIDint);
+        getline(line_stream, dropoffID, ",");
+        int dropoffIDint = stoi(dropoffID);
+        tripInfo.push_back(dropoffIDint);
+
+        if(treeSelected == true)
+            treeMap[pickup] = tripInfo;
+        else
+            hashMap[pickup] = tripInfo;
+    }
+
+    uberData.close();
+}
