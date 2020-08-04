f9af4fe41fe2bc30e873dc72817dd3d9c7272b41
Michael Ivanov <ivanovmichael@ufl.edu>
2020-08-04 15:37:23 -0400

    made find function return bool

--- HashMap.h
+++ HashMap.h
@@ -10,17 +10,24 @@
 
 template<typename K, typename D>
 class HashMap {
-    std::vector<std::list<std::pair<K,D>>> hashTable;
+    std::vector<std::list<std::pair<K, D>>> hashTable;
     int count{};
     double loadFactor{};
+
     void resize();
+
 public:
     HashMap();
+
     explicit HashMap(int capacity);
+
     explicit HashMap(int capacity, double _loadFactor);
+
     D &operator[](K Key);
+
     void insert(K key, D data);
-    D find(K Key);
+
+    bool find(K Key);
 };
 
 template<typename K, typename D>
@@ -42,23 +49,23 @@ HashMap<K, D>::HashMap(int capacity, double _loadFactor) : count(0) {
 template<typename K, typename D>
 void HashMap<K, D>::insert(K key, D data) {
     int hashCode = key % hashTable.size();
-    if(hashTable[hashCode].empty()) {
+    if (hashTable[hashCode].empty()) {
         count++;
     }
     hashTable[hashCode].push_front(std::make_pair(key, data));
-    if(((double) count) / hashTable.size() > loadFactor)
+    if (((double) count) / hashTable.size() > loadFactor)
         resize(); // size is doubled if load factor is met
 }
 
 template<typename K, typename D>
 void HashMap<K, D>::resize() {
-    std::vector<std::list<std::pair<K,D>>> newTable;
+    std::vector<std::list<std::pair<K, D>>> newTable;
     newTable.resize(hashTable.size() * 2);
     count = 0;
-    for(auto &list : hashTable){
-        for(auto &e : list){
+    for (auto &list : hashTable) {
+        for (auto &e : list) {
             int hashcode = e.first % newTable.size();
-            if(newTable[hashcode].empty()) {
+            if (newTable[hashcode].empty()) {
                 count++;
             }
             newTable[hashcode].push_front(std::make_pair(e.first, e.second));
@@ -71,8 +78,8 @@ template<typename K, typename D>
 D &HashMap<K, D>::operator[](K Key) {
     int hashcode = Key % hashTable.size();
     auto &list = hashTable[hashcode];
-    for(auto &e : list){
-        if(e.first == Key){
+    for (auto &e : list) {
+        if (e.first == Key) {
             return e.second;
         }
     }
@@ -81,15 +88,15 @@ D &HashMap<K, D>::operator[](K Key) {
 }
 
 template<typename K, typename D>
-D HashMap<K, D>::find(K Key) {
+bool HashMap<K, D>::find(K Key) {
     int hashcode = Key % hashTable.size();
     auto &list = hashTable[hashcode];
-    for(auto &e : list){
-        if(e.first == Key){
-            return e.second;
+    for (auto &e : list) {
+        if (e.first == Key) {
+            return true;
         }
     }
-    return D();
+    return false;
 }
 
 
