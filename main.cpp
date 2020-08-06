71f1632839c422890e6ee70f62d80a5e0b8042e6
Michael Ivanov <ivanovmichael@ufl.edu>
2020-08-05 20:56:27 -0400

    Added more error checking

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main() {
             switch(displaySelection) {
 
                 case 0 :
-                    cout << "Thank you for using Uber vs Taxi Data Visualization Experience!"  << endl;
+                    cout << "Thank you for using Uber vs Yellow Taxi Data Visualization Experience!"  << endl;
                     exit = true;
                     break;
                 case 1 :
@@ -84,6 +84,8 @@ int main() {
                     cin >> neighborhood;
                     Display::perNeighborhoodTree(taxiTreeMap, uberTreeMap, neighborhood);
                     break;
+                default:
+                    cout << "Please select a correct value" << endl;
             }
 
         }
@@ -113,6 +115,8 @@ int main() {
                     cin >> neighborhood;
                     Display::perNeighborhoodHash(taxiHashMap, uberHashMap, neighborhood);
                     break;
+                default:
+                    cout << "Please select a correct value" << endl;
             }
         }
     }
@@ -122,108 +126,134 @@ int main() {
 
 void readTaxiData(bool selection, TreeMap<long long,vector<long long>> &treeMap,  HashMap<long long,vector<long long>> &hashMap){
     ifstream taxiData("TaxiData.csv");
-    string line;
-    istringstream line_stream;
+    if(taxiData.is_open()) {
+        string line;
+        istringstream line_stream;
 
-    while(!taxiData.eof()) {
-        string pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix, token;
-        vector<long long> tripInfo;
-        getline(taxiData, line, '\n');
-        line_stream.str(line);
-        getline(line_stream,token, ',');
-        getline(line_stream, pickupFull, ',');
+        while (!taxiData.eof()) {
+            string pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix, token;
+            long long pickup, dropoff, pickupIDlong, dropoffIDlong;
+            vector<long long> tripInfo;
+            getline(taxiData, line, '\n');
+            line_stream.str(line);
+            getline(line_stream, token, ',');
+            getline(line_stream, pickupFull, ',');
 
 
-        for (int j = 0; j < pickupFull.length(); j++) {
-            if (pickupFull[j] != '-' && pickupFull[j] != ' ' && pickupFull[j] != ':') {
-                pickupFix += pickupFull[j];
+            for (int j = 0; j < pickupFull.length(); j++) {
+                if (pickupFull[j] != '-' && pickupFull[j] != ' ' && pickupFull[j] != ':') {
+                    pickupFix += pickupFull[j];
+                }
             }
-        }
-        long long pickup = stoll(pickupFix);
-
-        getline(line_stream, dropoffFull, ',');
-        for (int k = 0; k < dropoffFull.length(); k++) {
-            if (dropoffFull[k] != '-' && dropoffFull[k] != ' ' && dropoffFull[k] != ':') {
-                dropoffFix += dropoffFull[k];
+            try {
+                pickup = stoll(pickupFix);
+            } catch (invalid_argument &) {
+                pickup = 0;
             }
-        }
-        long long dropoff = stoll(dropoffFix);
-        tripInfo.push_back(dropoff);
-        getline(line_stream,token, ',');
-        getline(line_stream,token, ',');
-        getline(line_stream,token, ',');
-        getline(line_stream,token, ',');
-        getline(line_stream, pickupID, ',');
-        long long pickupIDlong= stoll(pickupID);
-        tripInfo.push_back(pickupIDlong);
-        getline(line_stream, dropoffID, ',');
-        long long dropoffIDlong= stoll(dropoffID);
-        tripInfo.push_back(dropoffIDlong);
 
-        if(selection)
-            treeMap[pickup] = tripInfo;
-        else
-            hashMap[pickup] = tripInfo;
+            getline(line_stream, dropoffFull, ',');
+            for (int k = 0; k < dropoffFull.length(); k++) {
+                if (dropoffFull[k] != '-' && dropoffFull[k] != ' ' && dropoffFull[k] != ':') {
+                    dropoffFix += dropoffFull[k];
+                }
+            }
+            try {
+                dropoff = stoll(dropoffFix);
+            } catch (invalid_argument &) {
+                dropoff = 0;
+            }
+            tripInfo.push_back(dropoff);
+            getline(line_stream, token, ',');
+            getline(line_stream, token, ',');
+            getline(line_stream, token, ',');
+            getline(line_stream, token, ',');
+            getline(line_stream, pickupID, ',');
+            try {
+                pickupIDlong = stoll(pickupID);
+            } catch (invalid_argument &) {
+                pickupIDlong = 264;
+            }
+            tripInfo.push_back(pickupIDlong);
+            getline(line_stream, dropoffID, ',');
+            try {
+                dropoffIDlong = stoll(dropoffID);
+            } catch (invalid_argument &) {
+                dropoffIDlong = 264;
+            }
+            tripInfo.push_back(dropoffIDlong);
+
+            if (selection)
+                treeMap[pickup] = tripInfo;
+            else
+                hashMap[pickup] = tripInfo;
+        }
+
+        taxiData.close();
     }
-
-    taxiData.close();
 }
 
 void readUberData(bool selection, TreeMap<long long, vector<long long>> &treeMap, HashMap<long long, vector<long long>> &hashMap) {
     ifstream uberData("UberData.csv");
-    string line;
-    istringstream line_stream;
+    if(uberData.is_open()) {
+        string line;
+        istringstream line_stream;
 
-    while(!uberData.eof()) {
-        string  dispatchBase, pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix;
-        vector<long long> tripInfo;
-        getline(uberData, line, '\n');
-        line_stream.str(line);
+        while (!uberData.eof()) {
+            string dispatchBase, pickupFull, dropoffFull, pickupID, dropoffID, pickupFix, dropoffFix;
+            long long pickup, dropoff, pickupIDlong, dropoffIDlong;
+            vector<long long> tripInfo;
+            getline(uberData, line, '\n');
+            line_stream.str(line);
 
-        getline(line_stream, dispatchBase, ',');
-        getline(line_stream, pickupFull, ',');
+            getline(line_stream, dispatchBase, ',');
+            getline(line_stream, pickupFull, ',');
 
-        for (int j = 0; j < pickupFull.length(); j++) {
-            if (pickupFull[j] != '-' && pickupFull[j] != ' ' && pickupFull[j] != ':') {
-                pickupFix += pickupFull[j];
+            for (int j = 0; j < pickupFull.length(); j++) {
+                if (pickupFull[j] != '-' && pickupFull[j] != ' ' && pickupFull[j] != ':') {
+                    pickupFix += pickupFull[j];
+                }
             }
-        }
-
-        long long pickup = stoll(pickupFix);
-        getline(line_stream, dropoffFull, ',');
-        for (int k = 0; k < dropoffFull.length(); k++) {
-            if (dropoffFull[k] != '-' && dropoffFull[k] != ' ' && dropoffFull[k] != ':') {
-                dropoffFix += dropoffFull[k];
+            try {
+                pickup = stoll(pickupFix);
+            } catch (invalid_argument &) {
+                pickup = 0;
             }
+            getline(line_stream, dropoffFull, ',');
+            for (int k = 0; k < dropoffFull.length(); k++) {
+                if (dropoffFull[k] != '-' && dropoffFull[k] != ' ' && dropoffFull[k] != ':') {
+                    dropoffFix += dropoffFull[k];
+                }
+            }
+            try {
+                dropoff = stoll(dropoffFix);
+            } catch (invalid_argument &) {
+                dropoff = 0;
+            }
+            tripInfo.push_back(dropoff);
+            getline(line_stream, pickupID, ',');
+            try {
+                pickupIDlong = stoll(pickupID);
+            } catch (invalid_argument &) {
+                pickupIDlong = 264;
+            }
+
+            tripInfo.push_back(pickupIDlong);
+            getline(line_stream, dropoffID, ',');
+
+            try {
+                dropoffIDlong = stoll(pickupID);
+            } catch (invalid_argument &) {
+                dropoffIDlong = 264;
+            }
+
+            tripInfo.push_back(dropoffIDlong);
+
+            if (selection)
+                treeMap[pickup] = tripInfo;
+            else
+                hashMap[pickup] = tripInfo;
         }
-        long long dropoff = stoll(dropoffFix);
-        tripInfo.push_back(dropoff);
-        getline(line_stream, pickupID, ',');
-        long long pickupIDlong;
 
-        try {
-            pickupIDlong = stoll(pickupID);
-        } catch (invalid_argument) {
-            pickupIDlong = 264;
-        }
-
-        tripInfo.push_back(pickupIDlong);
-        getline(line_stream, dropoffID, ',');
-
-        long long dropoffIDlong;
-        try {
-            dropoffIDlong = stoll(pickupID);
-        } catch (invalid_argument) {
-            dropoffIDlong = 264;
-        }
-
-        tripInfo.push_back(dropoffIDlong);
-
-        if (selection)
-            treeMap[pickup] = tripInfo;
-        else
-            hashMap[pickup] = tripInfo;
+        uberData.close();
     }
-
-    uberData.close();
 }
